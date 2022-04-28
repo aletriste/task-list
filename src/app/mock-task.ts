81e84f060cf8f011b1f2798499b544dff0e10293
aletriste <atrsitezza@gmail.com>
2022-04-27 21:44:25 -0300

    task interface y mock task creado

---
+++ src/app/mock-task.ts
@@ -0,0 +1,21 @@
+import { Task } from "./Task"
+export const TASK : Task[]= [
+    {
+        id :1,
+        text: "Terminar el primer modulo",
+        day: "agosto 13",
+        reminder: true
+    },
+    {
+        id :2,
+        text: "estudiar Angular",
+        day: "martes 13",
+        reminder: false
+    },
+    {
+        id :2,
+        text: "darle a Java",
+        day: "jueves 17",
+        reminder: false
+    },
+] 
