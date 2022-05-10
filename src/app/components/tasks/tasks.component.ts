03bc8657c3d98c0ea896584d620292fc52eb12fe
aletriste <atrsitezza@gmail.com>
2022-05-10 18:38:21 -0300

    servidor remoto implementado

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/service/task.service';
 import { TASK } from 'src/app/mock-task';
 import { Task } from 'src/app/Task';
 
@@ -8,10 +9,13 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  task :Task[] = TASK;
-  constructor() { }
+  task :Task[] = [];
+  constructor(private taskService :TaskService) {
+    
+   }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((task)=>(this.task = task));
   }
 
 }
---
+++ src/app/service/task.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { TASK } from '../mock-task';
+import { Task } from '../Task';
+import { Observable, of } from 'rxjs';
+import {HttpClient, HttpHandler} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl ="http://localhost:3000/task"
+  constructor(private http:HttpClient) { }
+  getTasks(): Observable<Task[]>{
+    
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+}
